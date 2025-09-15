77f7e662cb38fc07ece3b514e6559ef8baf4d2a1
Jason Madar <env3d@users.noreply.github.com>
2025-09-15 19:52:26 +0000

    update

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -7,7 +7,7 @@ export LLAMA_CPP_LIB=/workspaces/$(basename $(pwd))/bin/libllama.so
 # smallest qwen model
 wget https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q2_k.gguf
 
-nohub bin/llama-server -m qwen2.5-0.5b-instruct-q2_k.gguf & 
+nohup bin/llama-server -m qwen2.5-0.5b-instruct-q2_k.gguf & 
 
 echo ""
 echo "✅ DevContainer setup complete!"
