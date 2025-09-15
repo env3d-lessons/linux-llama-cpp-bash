7b5def3232c8efcfa7fc492ed2ca3f789427890e
Jason Madar <env3d@users.noreply.github.com>
2025-09-15 17:12:32 +0000

    update path

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "export LLAMA_CPP_LIB_PATH=/workspaces/$(basename $(pwd))/bin/" >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/workspaces/'"$(basename $(pwd))"'/bin/:$LD_LIBRARY_PATH' >> ~/.bashrc
+export LLAMA_CPP_LIB=/workspaces/$(basename $(pwd))/bin/libllama.so
+
 # smallest qwen model
 wget https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q2_k.gguf
 
