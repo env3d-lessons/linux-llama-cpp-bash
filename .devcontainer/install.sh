f688c92cd5c7be2a56e041cd094cef374d39ddcf
Jason Madar <env3d@users.noreply.github.com>
2025-09-15 20:02:06 +0000

    update

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -4,10 +4,14 @@ echo "export LLAMA_CPP_LIB_PATH=/workspaces/$(basename $(pwd))/bin/" >> ~/.bashr
 echo 'export LD_LIBRARY_PATH=/workspaces/'"$(basename $(pwd))"'/bin/:$LD_LIBRARY_PATH' >> ~/.bashrc
 export LLAMA_CPP_LIB=/workspaces/$(basename $(pwd))/bin/libllama.so
 
+echo "Updating..."
+sudo apt update
+
+echo "Installing packages..."
+sudo apt install supervisor
+
 # smallest qwen model
 wget https://huggingface.co/Qwen/Qwen2.5-0.5B-Instruct-GGUF/resolve/main/qwen2.5-0.5b-instruct-q2_k.gguf
-
-nohup bin/llama-server -m qwen2.5-0.5b-instruct-q2_k.gguf & 
 
 echo ""
 echo "✅ DevContainer setup complete!"
