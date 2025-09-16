dbd7519ed86b721b881d780c1173dd5fb48d9a82
Jason Madar <env3d@users.noreply.github.com>
2025-09-16 17:18:25 +0000

    remove solution

--- query.sh
+++ query.sh
@@ -1,15 +1,8 @@
-# Check if an argument is provided
-if [ -z "$1" ]; then
-  echo "Error"
-  echo "Usage: $0 <prompt>"
-  exit 1
-fi
-
 curl -s http://127.0.0.1:8080/v1/chat/completions \
   -H "Content-Type: application/json" \
   -d "{
     \"model\": \"qwen2.5-0.5b-instruct\",
-    \"messages\": [{\"role\": \"user\", \"content\": \"$1\"}],
+    \"messages\": [{\"role\": \"user\", \"content\": \"Write me a poem on data formats\"}],
     \"stream\": true,
     \"max_tokens\": 256
-  }" | grep '{\"content' | cut -f 12 -d '"' | tr -d '\n' | sed 's/\\n/\n/g' 
+  }" 
